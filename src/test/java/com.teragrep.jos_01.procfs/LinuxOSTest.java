3a0043d9c36fae2cb5cd4ccc78eabd128597d7c0
p000052u <140611897+Laukkala@users.noreply.github.com>
2024-06-07 07:42:25 +0300

    Added tests to LinuxOSTest, should cover all current methods. Edited tests to be system-independent

--- src/test/java/com.teragrep.jos_01.procfs/LinuxOSTest.java
+++ src/test/java/com.teragrep.jos_01.procfs/LinuxOSTest.java
@@ -52,14 +52,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class LinuxOSTest {
 
     private final Logger LOGGER = LoggerFactory.getLogger(LinuxOSTest.class);
 
+    // Uptime should have 2 fields, and the values should be positive floats.
     @Test
     public void upTimeTest() throws IOException {
-
         LinuxOS os = new LinuxOS();
         Uptime uptime = os.uptime();
         Assertions.assertEquals(2, uptime.statistics().size());
@@ -67,6 +68,7 @@ public class LinuxOSTest {
         Assertions.assertTrue(Float.parseFloat(uptime.statistics().get("combinedCpuCoreIdleTimeSeconds")) > 0);
     }
 
+    // Stat should have 12 fields, and all expected keys should be present.
     @Test
     public void statTest() throws IOException {
 
@@ -93,6 +95,211 @@ public class LinuxOSTest {
         }
     }
 
+    // Check that vmStat has all expected keys, and does not contain any unexpected keys.
+    // VMStat should have 187 fields
+    @Test
+    public void vmStatTest() throws IOException {
+        LinuxOS os = new LinuxOS();
+        Vmstat vmstat = os.vmstat();
+
+        Assertions.assertEquals(178,vmstat.statistics().size());
+        String[] expectedKeys = {
+                "nr_free_pages",
+                "nr_zone_inactive_anon",
+                "nr_zone_active_anon",
+                "nr_zone_inactive_file",
+                "nr_zone_active_file",
+                "nr_zone_unevictable",
+                "nr_zone_write_pending",
+                "nr_mlock",
+                "nr_bounce",
+                "nr_zspages",
+                "nr_free_cma",
+                "numa_hit",
+                "numa_miss",
+                "numa_foreign",
+                "numa_interleave",
+                "numa_local",
+                "numa_other",
+                "nr_inactive_anon",
+                "nr_active_anon",
+                "nr_inactive_file",
+                "nr_active_file",
+                "nr_unevictable",
+                "nr_slab_reclaimable",
+                "nr_slab_unreclaimable",
+                "nr_isolated_anon",
+                "nr_isolated_file",
+                "workingset_nodes",
+                "workingset_refault_anon",
+                "workingset_refault_file",
+                "workingset_activate_anon",
+                "workingset_activate_file",
+                "workingset_restore_anon",
+                "workingset_restore_file",
+                "workingset_nodereclaim",
+                "nr_anon_pages",
+                "nr_mapped",
+                "nr_file_pages",
+                "nr_dirty",
+                "nr_writeback",
+                "nr_writeback_temp",
+                "nr_shmem",
+                "nr_shmem_hugepages",
+                "nr_shmem_pmdmapped",
+                "nr_file_hugepages",
+                "nr_file_pmdmapped",
+                "nr_anon_transparent_hugepages",
+                "nr_vmscan_write",
+                "nr_vmscan_immediate_reclaim",
+                "nr_dirtied",
+                "nr_written",
+                "nr_throttled_written",
+                "nr_kernel_misc_reclaimable",
+                "nr_foll_pin_acquired",
+                "nr_foll_pin_released",
+                "nr_kernel_stack",
+                "nr_page_table_pages",
+                "nr_sec_page_table_pages",
+                "nr_swapcached",
+                "pgpromote_success",
+                "pgpromote_candidate",
+                "nr_dirty_threshold",
+                "nr_dirty_background_threshold",
+                "pgpgin",
+                "pgpgout",
+                "pswpin",
+                "pswpout",
+                "pgalloc_dma",
+                "pgalloc_dma32",
+                "pgalloc_normal",
+                "pgalloc_movable",
+                "pgalloc_device",
+                "allocstall_dma",
+                "allocstall_dma32",
+                "allocstall_normal",
+                "allocstall_movable",
+                "allocstall_device",
+                "pgskip_dma",
+                "pgskip_dma32",
+                "pgskip_normal",
+                "pgskip_movable",
+                "pgskip_device",
+                "pgfree",
+                "pgactivate",
+                "pgdeactivate",
+                "pglazyfree",
+                "pgfault",
+                "pgmajfault",
+                "pglazyfreed",
+                "pgrefill",
+                "pgreuse",
+                "pgsteal_kswapd",
+                "pgsteal_direct",
+                "pgsteal_khugepaged",
+                "pgdemote_kswapd",
+                "pgdemote_direct",
+                "pgdemote_khugepaged",
+                "pgscan_kswapd",
+                "pgscan_direct",
+                "pgscan_khugepaged",
+                "pgscan_direct_throttle",
+                "pgscan_anon",
+                "pgscan_file",
+                "pgsteal_anon",
+                "pgsteal_file",
+                "zone_reclaim_failed",
+                "pginodesteal",
+                "slabs_scanned",
+                "kswapd_inodesteal",
+                "kswapd_low_wmark_hit_quickly",
+                "kswapd_high_wmark_hit_quickly",
+                "pageoutrun",
+                "pgrotated",
+                "drop_pagecache",
+                "drop_slab",
+                "oom_kill",
+                "numa_pte_updates",
+                "numa_huge_pte_updates",
+                "numa_hint_faults",
+                "numa_hint_faults_local",
+                "numa_pages_migrated",
+                "pgmigrate_success",
+                "pgmigrate_fail",
+                "thp_migration_success",
+                "thp_migration_fail",
+                "thp_migration_split",
+                "compact_migrate_scanned",
+                "compact_free_scanned",
+                "compact_isolated",
+                "compact_stall",
+                "compact_fail",
+                "compact_success",
+                "compact_daemon_wake",
+                "compact_daemon_migrate_scanned",
+                "compact_daemon_free_scanned",
+                "htlb_buddy_alloc_success",
+                "htlb_buddy_alloc_fail",
+                "cma_alloc_success",
+                "cma_alloc_fail",
+                "unevictable_pgs_culled",
+                "unevictable_pgs_scanned",
+                "unevictable_pgs_rescued",
+                "unevictable_pgs_mlocked",
+                "unevictable_pgs_munlocked",
+                "unevictable_pgs_cleared",
+                "unevictable_pgs_stranded",
+                "thp_fault_alloc",
+                "thp_fault_fallback",
+                "thp_fault_fallback_charge",
+                "thp_collapse_alloc",
+                "thp_collapse_alloc_failed",
+                "thp_file_alloc",
+                "thp_file_fallback",
+                "thp_file_fallback_charge",
+                "thp_file_mapped",
+                "thp_split_page",
+                "thp_split_page_failed",
+                "thp_deferred_split_page",
+                "thp_split_pmd",
+                "thp_scan_exceed_none_pte",
+                "thp_scan_exceed_swap_pte",
+                "thp_scan_exceed_share_pte",
+                "thp_split_pud",
+                "thp_zero_page_alloc",
+                "thp_zero_page_alloc_failed",
+                "thp_swpout",
+                "thp_swpout_fallback",
+                "balloon_inflate",
+                "balloon_deflate",
+                "balloon_migrate",
+                "swap_ra",
+                "swap_ra_hit",
+                "ksm_swpin_copy",
+                "cow_ksm",
+                "zswpin",
+                "zswpout",
+                "direct_map_level2_splits",
+                "direct_map_level3_splits",
+                "nr_unstable"
+        };
+        for (String key : expectedKeys) {
+            Assertions.assertTrue(vmstat.statistics().containsKey(key));
+        }
+
+        for (Map.Entry entry : vmstat.statistics().entrySet()) {
+            boolean isExpected = false;
+            for (String key : expectedKeys) {
+                if (key.equals(entry.getKey())) {
+                    isExpected = true;
+                    break;
+                }
+            }
+            Assertions.assertTrue(isExpected);
+        }
+    }
+
+    // CPUInfo should have 27 fields for every logical cpu (cpuThread) listed. Fields should be appended with an underscore and an index number
     @Test
     public void cpuInfoTest() throws IOException {
         LinuxOS os = new LinuxOS();
@@ -134,6 +341,7 @@ public class LinuxOSTest {
         }
     }
 
+    // MemInfo should have exactly 56 fields and should contain all the expected keys.
     @Test
     public void memInfoTest() throws IOException {
         LinuxOS os = new LinuxOS();
@@ -203,13 +411,22 @@ public class LinuxOSTest {
         }
     }
 
+    // Check that cpuCount and physicalCoreCount returns a positive integer
+    // PageSize and totalRAM should return a positive float
+    // TotalRAM should never exceed memory allocated to JVM
+    // CpuThreadCount matches Runtime.availableProcessors()
+    // CpuThreadCount should either match or be greater than the number of physical cores
+    // CpuThreadCount and number of physical cores should be greater or equal to the number of CPUs installed
     @Test
     public void highLevelMethodsTest() throws IOException {
         LinuxOS os = new LinuxOS();
-        Assertions.assertEquals(1, os.cpuCount());
-        Assertions.assertEquals(4, os.cpuThreadCount());
-        Assertions.assertEquals(4, os.cpuPhysicalCoreCount());
-        Assertions.assertEquals(4.0, os.pageSize(), 0.1);
-        Assertions.assertEquals(32790864, os.totalRAM(), 8);
+        Assertions.assertTrue(Integer.signum(os.cpuCount()) == 1);
+        Assertions.assertEquals(Runtime.getRuntime().availableProcessors(), os.cpuThreadCount());
+        Assertions.assertTrue(Integer.signum(os.cpuPhysicalCoreCount()) == 1);
+        Assertions.assertTrue(os.cpuThreadCount() >= os.cpuPhysicalCoreCount());
+        Assertions.assertTrue(os.cpuThreadCount() >= os.cpuCount() && os.cpuPhysicalCoreCount() >= os.cpuCount());
+        Assertions.assertTrue(Integer.signum(os.cpuCount()) == 1);
+        Assertions.assertTrue(os.pageSize() > 0);
+        Assertions.assertTrue(os.totalRAM() > Runtime.getRuntime().maxMemory() / 1000);
     }
 }
